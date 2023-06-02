e41fc598a1dc14c633f4d1c31e0bcaf74b60c5d1
AlinaDJ <130125422+AlinaDJ@users.noreply.github.com>
2023-06-02 09:45:20 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# podcast
+# Nuray
---
+++ index.js
@@ -0,0 +1,16 @@
+function Function() {
+    document.getElementById("demo").style.backgroundColor = "green";
+    document.getElementById("demo").innerHTML ="Submitted Successfully!";
+}
+
+function Function1() {
+    document.getElementById("now").innerHTML ="CONTACT US NOW!";
+    document.getElementById("now").style.color = "pink";
+    document.getElementById('now').style.fontSize='50px' ;
+}
+
+function Function2() {
+    document.getElementById("follow").style.backgroundColor = "blue";
+    document.getElementById("follow").style.borderBlockColor = "blue";
+}
+
